6c7a8e42a4ecb20bb75ebf2e42c08285ac1cc384
rain <1516587005@qq.com>
2024-06-30 15:38:16 +0800

    V-02

--- src/main/java/com/example/micoutput/Menu.java
+++ src/main/java/com/example/micoutput/Menu.java
@@ -2,7 +2,6 @@ package com.example.micoutput;
 
 import com.example.micoutput.Kafka.KafkaProducer;
 import com.example.micoutput.RabbitMQ.RabbitMQProducer;
-import com.example.micoutput.controller.OrderController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
